7261cf56129e004c4a9cf58903c857bf20a6d8ca
00209317 <00209317@uca.edu.sv>
2019-03-23 00:59:39 -0600

    añadiendo camhios y add btnshare

--- app/src/main/java/com/example/labo3/Main2Activity.java
+++ app/src/main/java/com/example/labo3/Main2Activity.java
@@ -3,11 +3,13 @@ package com.example.labo3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     private TextView txv1,txv2,txv3,txv4;
+    private Button mShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,8 @@ public class Main2Activity extends AppCompatActivity {
         txv2 = findViewById(R.id.tv2);
         txv3 = findViewById(R.id.tv3);
         txv4 = findViewById(R.id.tv4);
+        mShare = findViewById(R.id.btnshare);
+
 
         Intent mIntent = getIntent();
         if (mIntent!=null){
@@ -26,5 +30,27 @@ public class Main2Activity extends AppCompatActivity {
             txv3.setText(mIntent.getStringExtra("key3"));
             txv4.setText(mIntent.getStringExtra("key4"));
         }
+
+
+        mShare.setOnClickListener(v -> {
+            String text1 = txv1.getText().toString();
+            String text2 = txv2.getText().toString();
+            String text3 = txv3.getText().toString();
+            String text4 = txv4.getText().toString();
+
+            Intent mIn = new Intent();
+            mIn.setType("Text/plain");
+            mIn.setAction(Intent.ACTION_SEND);
+            mIn.putExtra(Intent.EXTRA_TEXT, text1);
+            mIn.putExtra(Intent.EXTRA_TEXT, text2);
+            mIn.putExtra(Intent.EXTRA_TEXT, text3);
+            mIn.putExtra(Intent.EXTRA_TEXT, text4);
+            startActivity(mIn);
+
+        });
+
+
+
+
     }
 }
